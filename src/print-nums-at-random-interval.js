a832851c99893be8b914fb2e412fc8eaf0041f65
Ilya Levin <bskton@gmail.com>
2019-05-02 14:48:14 +1000

    Styling comments

--- src/print-nums-at-random-interval.js
+++ src/print-nums-at-random-interval.js
@@ -1,8 +1,8 @@
 /**
- Let’s assume that we have a for loop that prints 0 to 10 at random intervals (0 to n seconds).
- We need to modify it using promises to print sequentially 0 to 10.
- For example, if 0 takes 6 seconds to print and 1 takes two seconds to print,
- then 1 should wait for 0 to print and so on.
+ * Let’s assume that we have a for loop that prints 0 to 10 at random intervals (0 to n seconds).
+ * We need to modify it using promises to print sequentially 0 to 10.
+ * For example, if 0 takes 6 seconds to print and 1 takes two seconds to print,
+ * then 1 should wait for 0 to print and so on.
  */
 
 const wait = (i, ms) => new Promise(resolve => setTimeout(() => resolve(i), ms));
